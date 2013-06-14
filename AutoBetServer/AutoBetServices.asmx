960c9ed98c1b1adb399796552e8b7d44a1f24224
Duc Mai <teodam.de@gmail.com>
2013-06-14 09:16:48 -0700

    Create project

---
+++ AutoBetServer/AutoBetServices.asmx
@@ -0,0 +1,1 @@
+﻿<%@ WebService Language="C#" CodeBehind="AutoBetServices.asmx.cs" Class="AutoBetServer.AutoBetServices" %>
---
+++ AutoBetServer/AutoBetServices.asmx.cs
@@ -0,0 +1,27 @@
+﻿using System.Runtime.Remoting.Metadata;
+using System.Security.Cryptography.X509Certificates;
+using System.Web.Services;
+using AutoBetServer.Resource;
+
+namespace AutoBetServer
+{
+    [WebService(Namespace = "ns:autobet:server")]
+    public class AutoBetServices : WebService
+    {
+        [WebMethod]
+        [SoapMethod(SoapAction = "Hello")]
+        public string HelloWorld(string name)
+        {
+            return string.Format("Hello, my name is {0}", name);
+        }
+
+        [WebMethod]
+        [SoapMethod(SoapAction = "Authenticate")]
+        public bool Authenticate(string username, string password)
+        {
+            X509Certificate2 cert = new X509Certificate2(Resources.Autobet,"1");
+            
+            return true;
+        }
+    }
+}
